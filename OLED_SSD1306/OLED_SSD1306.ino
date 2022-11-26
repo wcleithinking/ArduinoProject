42f0193b298b1da3d36271dab7c16a6b26ead7ff
wcleithinking <wcleithinking@gmail.com>
2022-11-26 17:32:39 +0800

    update

--- OLED_SSD1306/OLED_SSD1306.ino
+++ OLED_SSD1306/OLED_SSD1306.ino
@@ -1,10 +1,25 @@
-# include <Wire.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-# include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH  128
+#define SCREEN_HEIGHT 64
+
+Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
 
+  if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    while (true);
+  }
+
+  delay(2000);
+
+  oled.clearDisplay();
+  
 }
 
 void loop() {
--- device_type.md
+++ device_type.md
@@ -2,6 +2,7 @@
 
 ## OLED Display
 1. Type: SSD1306
+    + https://arduinogetstarted.com/tutorials/arduino-oled
 2. Dislay: 128x32
 3. $I^2C$ address: 0x3C
 
